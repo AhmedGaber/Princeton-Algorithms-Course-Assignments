904a2cb1a30249d0476b2099ddcb579be678f247
AhmedGaber <ahmedgaber20@gmail.com>
2014-08-01 04:12:58 +0200

    Adding unimplemented methods to Board

--- Assignment4/src/Board.java
+++ Assignment4/src/Board.java
@@ -1,4 +1,75 @@
-
+/**
+ * 
+ * @author Ahmed Gaber
+ * 
+ */
 public class Board {
+    /**
+     * Construct a board from an N-by-N array of blocks (where blocks[i][j] =
+     * block in row i, column j)
+     * 
+     * @param blocks
+     */
+    public Board(int[][] blocks) {
+    }
 
+    /**
+     * board dimension N
+     * 
+     * @return
+     */
+    public int dimension() {
+    }
+
+    /**
+     * number of blocks out of place
+     * 
+     * @return
+     */
+    public int hamming() {
+    }
+
+    /**
+     * sum of Manhattan distances between blocks and goal
+     * 
+     * @return
+     */
+    public int manhattan() {
+    }
+
+    /**
+     * is this board the goal board?
+     * 
+     * @return
+     */
+    public boolean isGoal() {
+    }
+
+    /**
+     * a board obtained by exchanging two adjacent blocks in the same row
+     * 
+     * @return
+     */
+    public Board twin() {
+    }
+
+    /**
+     * does this board equal y?
+     */
+    public boolean equals(Object y) {
+    }
+
+    /**
+     * all neighboring boards
+     * 
+     * @return
+     */
+    public Iterable<Board> neighbors() {
+    }
+
+    /**
+     * string representation of the board (in the output format specified below)
+     */
+    public String toString() {
+    }
 }
