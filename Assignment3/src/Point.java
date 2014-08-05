5d48f55c39306e081e4e1b38247a385c432387ca
AhmedGaber <ahmedgaber20@gmail.com>
2014-08-05 19:29:10 +0200

    Adding code comments

--- Assignment3/src/Point.java
+++ Assignment3/src/Point.java
@@ -3,48 +3,69 @@ import java.util.Comparator;
 public class Point implements Comparable<Point> {
 
     // compare points by slope
-    public final Comparator<Point> SLOPE_ORDER ; // YOUR DEFINITION HERE
+    public final Comparator<Point> SLOPE_ORDER = null; // YOUR DEFINITION HERE
 
     private final int x; // x coordinate
     private final int y; // y coordinate
 
-    // create the point (x, y)
+    /**
+     * Create the point (x, y)
+     * 
+     * @param x
+     * @param y
+     */
     public Point(int x, int y) {
         /* DO NOT MODIFY */
         this.x = x;
         this.y = y;
     }
 
-    // plot this point to standard drawing
+    /**
+     * Plot this point to standard drawing
+     */
     public void draw() {
-        /* DO NOT MODIFY */
         StdDraw.point(x, y);
     }
 
-    // draw line between this point and that point to standard drawing
+    /**
+     * Draw line between this point and that point to standard drawing
+     * 
+     * @param that
+     */
     public void drawTo(Point that) {
-        /* DO NOT MODIFY */
         StdDraw.line(this.x, this.y, that.x, that.y);
     }
 
-    // slope between this point and that point
+    /**
+     * Slope between this point and that point
+     * 
+     * @param that
+     * @return
+     */
     public double slopeTo(Point that) {
         /* YOUR CODE HERE */
     }
 
-    // is this point lexicographically smaller than that one?
-    // comparing y-coordinates and breaking ties by x-coordinates
+    /**
+     * Is this point lexicographically smaller than that one? comparing
+     * y-coordinates and breaking ties by x-coordinates
+     */
     public int compareTo(Point that) {
         /* YOUR CODE HERE */
     }
 
-    // return string representation of this point
+    /**
+     * Return string representation of this point
+     */
     public String toString() {
-        /* DO NOT MODIFY */
         return "(" + x + ", " + y + ")";
     }
 
-    // unit test
+    /**
+     * Unit test
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         /* YOUR CODE HERE */
     }
