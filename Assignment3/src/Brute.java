d01068366573f5dd7c89d415985ff67c06558be3
AhmedGaber <ahmedgaber20@gmail.com>
2014-08-05 21:18:24 +0200

    Adding unimplemented methods to Brute and Fast Classes

--- Assignment3/src/Brute.java
+++ Assignment3/src/Brute.java
@@ -1,7 +1,31 @@
 public class Brute {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        In in = new In();
+        Draw draw = new Draw();
+        draw.setXscale(0, 32768);
+        Out out = new Out();
+        Point[] points = getInput(in);
+        doLogic(points, draw, out);
+    }
+
+    private static Point[] getInput(In in) {
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            points[i] = new Point(in.readInt(), in.readInt());
+        }
+        return points;
+    }
+
+    private static void doLogic(Point[] points, Draw draw, Out out) {
+    }
+
+    private static void drawPoint() {
+
+    }
+
+    private static void drawLine() {
 
     }
 
