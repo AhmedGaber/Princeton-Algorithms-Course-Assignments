21b710de6ae461f3f2789b331df352fa63035453
AhmedGaber <ahmedgaber20@gmail.com>
2014-08-08 16:57:22 +0200

    Started Working on Fast Class

--- Assignment3/src/Fast.java
+++ Assignment3/src/Fast.java
@@ -4,40 +4,46 @@ public class Fast {
 
     public static void main(String[] args) {
         In in = new In();
-        Draw draw = new Draw();
-        draw.setXscale(0, 32768);
         Out out = new Out();
-        Point[] points = getInput(in);
-        doLogic(points, draw, out);
-    }
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        StdDraw.setPenRadius(0.004);
 
-    private static Point[] getInput(In in) {
         int n = in.readInt();
         Point[] points = new Point[n];
         for (int i = 0; i < n; i++) {
             points[i] = new Point(in.readInt(), in.readInt());
+            points[i].draw();
         }
-        return points;
+
+        doLogic(points, out);
     }
 
-    private static void doLogic(Point[] points, Draw draw, Out out) {
+    private static void doLogic(Point[] points, Out out) {
         double pqSlope = 0;
         double prSlope = 0;
         double psSlope = 0;
-        Arrays.sort(points);
-        
-    }
-
-    private static void drawPoint() {
 
     }
 
-    private static void drawLine() {
+    private static void printResult(Out out, Point[] points, int p, int q,
+            int r, int s) {
 
+        Point[] temp = { points[p], points[q], points[r], points[s] };
+        Arrays.sort(temp);
+        temp[0].drawTo(temp[3]);
+        out.println(temp[0].toString() + " -> " + temp[1].toString() + " -> "
+                + temp[2].toString() + " -> " + temp[3].toString());
     }
 
-    private static void printResult() {
-
-    }
-
+    /**
+     * Test Cases
+     * 
+     * 6 19000 10000 18000 10000 32000 10000 21000 10000 1234 5678 14000 10000
+     * 
+     * 8 10000 0 0 10000 3000 7000 7000 3000 20000 21000 3000 4000 14000 15000
+     * 6000 7000
+     * 
+     * 
+     */
 }
